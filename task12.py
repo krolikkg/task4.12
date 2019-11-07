080785d54480d79d1e0f95eb78001488ddbf39c5
krolikkg <thekalmuratov@mail.ru>
2019-11-07 12:52:08 +0600

    jdi

---
+++ task12.py
@@ -0,0 +1,17 @@
+def myDecorator(func):
+    def wrapper(*args, **kwargs):
+        print("Calling testFunck:", args, kwargs)
+        func(*args, **kwargs)
+
+    return wrapper
+
+@myDecorator
+def testFunc(a, b=1, *args, **kwargs):
+    print("argument a: ", a)
+    print("argument b: ", b)
+    print("argument args: ", args)
+    print("argument kwargs: ", kwargs)
+
+testFunc(1,2,3,4, c=6, d=7)
+print()
+testFunc(1, c=6)
